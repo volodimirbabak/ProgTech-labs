98aece7085caf00d3ed4f9bef611f17f3d4bbcdc
volodimirbabak <volodimirbabak502@gmail.com>
2025-12-23 23:09:47 +0200

    Update news_feed.py

--- news_feed.py
+++ news_feed.py
@@ -1,6 +1,6 @@
 class NewsFeed:
     def __init__(self):
-        # Ініціалізація черги нови
+        # Ініціалізація черги новин
         self._queue = []
 
     def add_news(self, title: str, content: str) -> bool:
